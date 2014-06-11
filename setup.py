4dcc2e26394c13ce6efc8099e264a4d28e15fda6
Russell Stewart <russell.s.stewart@gmail.com>
2014-06-10 23:41:57 -0700

    Relasing 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='pythonpy',
-    version='0.1.9dev',
+    version='0.2.0',
     description='Command line utility for Python',
     scripts=[os.path.join('bin', 'pythonpy')],
     license='MIT',
