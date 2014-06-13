8fff08222fe275c725c7841feba5e73e4927ae34
Russell Stewart <russell.s.stewart@gmail.com>
2014-06-12 22:57:01 -0700

    Removed long_description from pip in favor of url only.

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ import os
 setup(
     name='pythonpy',
     version='0.2.2dev2',
-    description='Command line utility for Python',
+    description='Take advantage of your python skills from the command line',
     scripts=[os.path.join('bin', 'pythonpy')],
     license='MIT',
-    long_description=open('README.txt').read(),
     url='https://github.com/Russell91/pythonpy',
 )
