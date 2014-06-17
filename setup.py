878f87ad77c7cc76a265b1b333395efcc9bfbacc
Russell Stewart <russell.s.stewart@gmail.com>
2014-06-16 19:33:46 -0700

    0.2.5dev2.

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import os
 
 setup(
     name='pythonpy',
-    version='0.2.5dev',
+    version='0.2.5dev2',
     description='Take advantage of your python skills from the command line',
-    scripts=['pythonpy', 'py'],
+    scripts=['pythonpy', 'py', 'wpy'],
     license='MIT',
     url='https://github.com/Russell91/pythonpy',
     long_description='',
