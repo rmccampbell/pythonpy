423d2354a8b4b84662593bfc6884bc4a6fb5bd51
Russell Stewart <russell.s.stewart@gmail.com>
2014-06-18 01:25:10 -0700

    0.2.6dev2

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from distutils.core import setup
 import os
 
 try:
-     with open('/etc/bash_completion.d/pycompletion.sh', 'w') eo:
+     with open('/etc/bash_completion.d/pycompletion.sh', 'w') as eo:
          eo.write('test')
      data_files = [('/etc/bash_completion.d', ['extras/pycompletion.sh']),]
-except error:
+except:
      print 'User does not have write access to /etc completion will not work'
      data_files = [('bash_completion.d', ['extras/pycompletion.sh']),]
 
 setup(
     name='pythonpy',
-    version='0.2.6dev',
+    version='0.2.6dev2',
     description='Take advantage of your python skills from the command line',
     scripts=['py', 'extras/pycompleter', 'extras/wpy'],
     data_files=data_files,
