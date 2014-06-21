ee5793d42e14bae3e5261f04b6cff8d7e970e126
Russell Stewart <russell.s.stewart@gmail.com>
2014-06-21 11:03:48 -0700

    Muted errors in pycompleter.

--- extras/pycompletion.sh
+++ extras/pycompletion.sh
@@ -1,7 +1,7 @@
 _py()
 {
     #COMPREPLY=($(echo "${COMP_WORDS[@]:1}" | pycompleter))
-    COMPREPLY=($(pycompleter "${COMP_WORDS[@]:1}" ))
+    COMPREPLY=($(pycompleter "${COMP_WORDS[@]:1}" 2>/dev/null ))
 }
 
 complete -F _py -o nospace py
