6b71ef0874eb12c34b1a50123f2b59d08666c7c6
Russell Stewart <russell.s.stewart@gmail.com>
2014-06-21 01:32:16 -0700

    0.3.1dev2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
 
 setup(
     name='pythonpy',
-    version='0.3.1dev',
+    version='0.3.1dev2',
     description='Take advantage of your python skills from the command line',
     scripts=['py', 'extras/py3', 'extras/pycompleter'],
     data_files=data_files,
