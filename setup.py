b190f2df87cd86cec6c742b375141a808de9cda0
Russell Stewart <russell.s.stewart@gmail.com>
2014-06-21 13:10:10 -0700

    0.3.1dev5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from the install directory.''')
 
 setup(
     name='pythonpy',
-    version='0.3.1dev4',
+    version='0.3.1dev5',
     description='Take advantage of your python skills from the command line',
     scripts=['py', 'extras/py3', 'extras/pycompleter'],
     data_files=data_files,
