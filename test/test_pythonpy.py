4398b6bee3e3a4e8955099e9ee6498ae756dde58
Finn Grimwood <fgrimwood@undo-software.com>
2014-06-25 19:16:09 +0100

    Added ability to import any python module

--- test/test_pythonpy.py
+++ test/test_pythonpy.py
@@ -26,26 +26,23 @@ class TestPythonPy(unittest.TestCase):
         self.assertEqual(check_output("""echo 3 | py -c 'a=5' -x x -C 'print(a)'""", shell=True), '3\n5\n')
 
     def test_imports(self):
-        check_output("py 'math'", shell=True)
-        check_output("py 'base64'", shell=True)
-        check_output("py 'calendar'", shell=True)
-        check_output("py 'csv'", shell=True)
-        check_output("py 'datetime'", shell=True)
-        check_output("py 'hashlib'", shell=True)
-        check_output("py 'glob'", shell=True)
-        check_output("py 'itertools'", shell=True)
-        check_output("py 'json'", shell=True)
-        check_output("py 'math'", shell=True)
-        check_output("py 'os'", shell=True)
-        check_output("py 'random'", shell=True)
-        check_output("py 're'", shell=True)
-        check_output("py 'shutil'", shell=True)
-        check_output("py 'tempfile'", shell=True)
-        check_output("py -c 'Counter'", shell=True)
-        check_output("py -c 'OrderedDict'", shell=True)
-        check_output("py -c 'groupby'", shell=True)
-        check_output("py 'uuid4'", shell=True)
-
+        module_commands = ["math.ceil(2.5)",
+                           "base64.b64encode('data to be encoded')",
+                           "calendar.weekday(1955, 11, 5)",
+                           "csv.list_dialects()",
+                           "datetime.timedelta(hours=-5)",
+                           "hashlib.sha224(\"Nobody inspects the spammish repetition\").hexdigest()",
+                           "glob.glob('*')",
+                           "itertools.product(['a','b'], [1,2])",
+                           "json.dumps([1,2,3,{'4': 5, '6': 7}], separators=(',',':'))",
+                           "os.name",
+                           "random.randint(0, 1000)",
+                           "re.compile('[a-z]').findall('abcd')",
+                           "shutil.get_archive_formats()",
+                           "tempfile.gettempdir()",
+                           "uuid.uuid1()"]
+        for command in module_commands:
+            check_output("py %r" % command, shell=True)
 
 if __name__ == '__main__':
     unittest.main()
