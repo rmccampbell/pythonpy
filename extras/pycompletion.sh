122dd4947b3e71a59c40c7fd7429ba7ee79d4851
Russell Stewart <Russell.s.stewart@gmail.com>
2014-06-26 16:31:51 -0700

    Changed bash completion to rely on ubiquitous _cd.

--- extras/pycompletion.sh
+++ extras/pycompletion.sh
@@ -1,43 +1,9 @@
-_pycd () 
-{ 
-    local cur prev words cword;
-    _init_completion || return;
-    local IFS='
-' i j k;
-    compopt -o filenames;
-    if [[ -z "${CDPATH:-}" || "$cur" == ?(.)?(.)/* ]]; then
-        _filedir -d;
-        return 0;
-    fi;
-    local -r mark_dirs=$(_rl_enabled mark-directories && echo y);
-    local -r mark_symdirs=$(_rl_enabled mark-symlinked-directories && echo y);
-    for i in ${CDPATH//:/'
-'};
-    do
-        k="${#COMPREPLY[@]}";
-        for j in $( compgen -d $i/$cur );
-        do
-            if [[ ( -n $mark_symdirs && -h $j || -n $mark_dirs && ! -h $j ) && ! -d ${j#$i/} ]]; then
-                j+="/";
-            fi;
-            COMPREPLY[k++]=${j#$i/};
-        done;
-    done;
-    _filedir -d;
-    if [[ ${#COMPREPLY[@]} -eq 1 ]]; then
-        i=${COMPREPLY[0]};
-        if [[ "$i" == "$cur" && $i != "*/" ]]; then
-            COMPREPLY[0]="${i}/";
-        fi;
-    fi;
-    return 0
-}
-
 _py()
 {
     COMPREPLY=($(pycompleter "${COMP_WORDS[@]}" 2>/dev/null ))
     if [[ ${COMPREPLY[0]} == '_pycd' ]]; then
-        _pycd
+        COMPREPLY=()
+        _cd 2>/dev/null
     fi
 }
 
