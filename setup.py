2115e8725130e184bedc51f51a646b429ab7e08e
Russell Stewart <russell.s.stewart@gmail.com>
2014-08-23 15:05:57 -0700

    0.3.5

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ for e in endings:
 
 setup(
     name='pythonpy',
-    version='0.3.4',
+    version='0.3.5',
     description='python -c, with tab completion and shorthand',
     data_files=data_files,
     license='MIT',
